94cfdb1ff2aaacb6175749df99c7d4803d00a644
phoeurnkimhor <phoeurnkimhor@gmail.com>
2025-12-26 08:45:03 +0700

    chnage global to page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Geist_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
-import "./globals.css"
   
 const geistMono = Geist_Mono({ subsets: ["latin"] })
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { AboutSection } from "@/components/about-section"
 import { SkillsSection } from "@/components/skills-section"
 import { ProjectsSection } from "@/components/projects-section"
 import { ContactSection } from "@/components/contact-section"
+import "./globals.css"
 
 export default function Home() {
   return (
